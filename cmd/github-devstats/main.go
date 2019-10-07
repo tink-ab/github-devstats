3b101ad7fd9ff09858996f0fad0421d4c8a48dc5
Kiril Videlov <kiril.videlov@tink.se>
2019-10-07 08:26:42 +0200

    Parametarize the day for which prs are fetched

--- cmd/github-devstats/main.go
+++ cmd/github-devstats/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"github.com/krlvi/github-devstats/client"
 	"github.com/krlvi/github-devstats/event"
+	"log"
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -13,10 +15,17 @@ func main() {
 	if len(org) <= 0 || len(accessToken) <= 0 {
 		panic("supply github organization and access token as command parameters")
 	}
+	daysAgo := 0
+	if len(os.Args) > 3 {
+		daysAgo, _ = strconv.Atoi(os.Args[3])
+	}
+
+	log.Println("fetching github teams and their members for", org)
 	c := client.NewClient(org, accessToken)
 
-	yesterday := time.Now().AddDate(0, 0, -1)
-	prs, reposByPR, err := c.GetMergedPRs(yesterday)
+	date := time.Now().AddDate(0, 0, daysAgo*-1)
+	log.Println("fetching merged pull requests for", org, "on date", date.Format("2006-01-02"))
+	prs, reposByPR, err := c.GetMergedPRs(date)
 	if err != nil {
 		panic("could not fetch pull requests")
 	}
